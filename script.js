c5562814098f4976ea6e348840aa2503ebefcc3e
s7illen <s7illen@gmail.com>
2019-12-17 00:57:13 -0500

    completed responsive design

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    window.scrollTo(0, 0);
+    // window.scrollTo(0, 0);
 
     $(window).scroll(function () { 
         if (whereAreWe("#contact")) { 
@@ -84,17 +84,17 @@ $(document).ready(function () {
 
 });
 
-$('.navProject').hover(console.log('hover'), console.log('not hover')
+// $('.navProject').hover(console.log('hover'), console.log('not hover')
     // $('nav ul li:nth-child(' + x + ')').replaceWith(
     //     function () {
     //         return $('<li class="navSelected"><i class="fas fa-dot-circle"></i></li>').hide().fadeIn('slow');
     //     }
     // )
 
-)
+// )
 
 const getDot = function (x , className) {
-    if ($('nav ul li:nth-child(' + x + ')').hasClass('navSelected')) {
+    if ($('nav ul a:nth-child(' + x + ') li').hasClass('navSelected')) {
     }
     else {
 
@@ -106,7 +106,7 @@ const getDot = function (x , className) {
         //     }
         // )
 
-        $('nav ul li:nth-child(' + x + ')').replaceWith(
+        $('nav ul a:nth-child(' + x + ') li').replaceWith(
             function () {
                 return $('<li class="navSelected ' + className + '"><i class="fas fa-dot-circle"></i></li>').hide().fadeIn('slow');
             }
@@ -114,13 +114,8 @@ const getDot = function (x , className) {
     }
 }
 
-$(".navHome").click(function () {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#home").offset().top
-    }, 300);
-});
 
-$(".mouse, .skillTitle, .navMark").click(function () {
+$(".mouse, .skillTitle").click(function () {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#mark").offset().top
     }, 300);
@@ -143,3 +138,70 @@ $(".contactTitle").click(function () {
         scrollTop: $("#contact").offset().top
     }, 300);
 });
+
+//submit form
+
+$(document).ready(function () {
+
+    const dbRef = firebase.database().ref();
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+    });
+
+    $(".send").click(function () {
+        const name = $(".contactName").val();
+        const email = $(".contactEmail").val();
+        const phone = $(".contactPhone").val();
+        const message = $(".message").val();
+        // $("#returnmessage").empty(); // To empty previous error/success message.
+        // Checking for blank fields.
+        if (name == '' || email == '' && phone =='') {
+            Swal.fire("Please Fill Your Name and Contact")
+        } else {
+            // Returns successful data submission message when the entered information is stored in database.
+
+            const contact = {
+                contactName: name,
+                contactEmail: email,
+                contactPhone: phone,
+                contactMessage: message,
+            };
+
+            dbRef.push(contact);
+
+            Swal.fire('Thanks! I will follow up soon!')
+        }
+    });
+});
+
+
+// $(document).ready(function () {
+
+//     const dbRef = firebase.database().ref();
+    
+//     let test = null;
+
+//     dbRef.on('value',(response) => {
+//         test = response.val();
+//         console.log(test)
+//     })
+    
+
+
+// });
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD9boplY7t43pbYJS3PLtbfdo_oS26jcow",
+    authDomain: "portfolio-e1e45.firebaseapp.com",
+    databaseURL: "https://portfolio-e1e45.firebaseio.com",
+    projectId: "portfolio-e1e45",
+    storageBucket: "portfolio-e1e45.appspot.com",
+    messagingSenderId: "975463387248",
+    appId: "1:975463387248:web:e0eb86b2ec3b16dba024b5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
